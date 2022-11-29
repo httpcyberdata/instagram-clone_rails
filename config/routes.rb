75d15aec53251b9d0ab86b7f311afb8fea257a42
httpcyberdata <borgeringstad@Brge-Ringstad-PC.local>
2022-11-28 20:13:44 -0500

    added uploader page /posts/new w/ a green button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonra ils.org/routing.html
   get "/dashboard" => "accounts#index"  
 
+  resources :posts, only: [:new, :create, :show]
+
   root to: 'public#homepage'
 
 end
